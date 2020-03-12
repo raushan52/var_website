36f54a40f03d0090be46fb227e3545e48da6e594
raushan52 <raushan52@hotmail.com>
2020-03-12 23:05:43 +0530

    working on php

---
+++ linked_pages/form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+        $name = $_POST["name"];
+        $email = $_POST["email"];
+        $phone = $_POST["phone"];
+        $message = $_POST["message"];
+
+    ?>
+</body>
+</html>
