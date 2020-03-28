944774db30a89df2101c4ad8416e0c467a17c00b
raushan52 <raushan52@hotmail.com>
2020-03-29 01:13:35 +0530

    Working on PHP

--- linked_pages/form.php
+++ linked_pages/form.php
@@ -3,18 +3,65 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Form PHP</title>
+    <title>Document</title>
 </head>
 <body>
-    <?php 
-        $name = $_POST["name"];
-        $email = $_POST["email"];
-        $phone = $_POST["phone"];
-        $message = $_POST["message"];
+<?php
+include 'config.php'; // store your configuration in a seperate file so 
+                      // you only need to update it once when your environment changes
 
-        echo ($name);
+$errors = false;
+$output = '';
+$nl = '<br>'.PHP_EOL;
+$redirect_url = 'after_form.html';
 
-    ?>
+if (!$con = new mysqli(DBHOST,DBUSER,DBPASS,DBNAME)){
+    $errors = true;
+    $output .= "ERROR Can't connect to DB".$nl;
+};   
 
+
+if (!$errors){
+   //should validate/clean $_POST before using in query
+   $name = $con->escape_string($_POST['your_name']);  
+   $email = $con->escape_string($_POST['your_email']);
+   $mobile = $con->escape_string($_POST['your_phone']);   
+   $slogan = $con->escape_string($_POST['your_message']);
+   $sql="INSERT INTO members 
+            (sName, sCity, sMobile, sEmail, sSub, sSlogan)
+         VALUES ('$name', '$city', '$mobile', '$email',
+                '$sub','$slogan')";
+
+   if (!$con->query($sql)){ //forgot a parenthesis here earlier
+      $output .= 'ERROR: DB said: ('.$con->errno.') '.$con->error.$nl;
+      $output .= 'Query was:'.$sql.$nl;
+      $errors = true;
+   }else{
+     $output .= "1 record added".$nl;
+   }
+}
+
+if (!$errors){
+   //if there are no errors redirect to index.html;
+   header('refresh: 2; URL='.$redirect_url);
+   $output .= '...Redirecting...'.$nl;
+}else{
+   //show the errors and allow display a link to go back/try again
+   $output .= '<a href="'.$redirect_url.'">Try again</a>'.$nl;
+}
+echo $output;
+?>
+
+<?php 
+
+pre_r($_POST);
+
+    function pre_r($array){
+        echo '<pre>';
+        print_r($array);
+        echo '</pre>';
+    }
+
+?>
 </body>
 </html>
